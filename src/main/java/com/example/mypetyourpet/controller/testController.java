d04dc5c7163ebb775f9d4befc6b778db6bd7d319
Xol Sucre <xochilt.sucre@gmail.com>
2025-10-29 22:51:29 -0700

    testing endpoint

---
+++ src/main/java/com/example/mypetyourpet/controller/testController.java
@@ -0,0 +1,16 @@
+package com.example.mypetyourpet.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/vl")
+public class testController {
+    @GetMapping("/testing")
+    public Map<String, String> testing() {
+        return Map.of("status", "OK", "version", "vl");
+    }
+}
