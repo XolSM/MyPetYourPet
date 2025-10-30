1f2134bb76bf4463f31b9ba8399281b260501523
Xol Sucre <xochilt.sucre@gmail.com>
2025-10-29 22:26:31 -0700

    first commit

---
+++ src/main/java/com/example/mypetyourpet/MyPetYourPetApplication.java
@@ -0,0 +1,13 @@
+package com.example.mypetyourpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetYourPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPetYourPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mypetyourpet/MyPetYourPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mypetyourpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPetYourPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
