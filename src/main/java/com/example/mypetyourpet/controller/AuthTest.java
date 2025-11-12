4630d997eb3955a9553a2125c49d1f7607df4546
Xol Sucre <xochilt.sucre@gmail.com>
2025-11-12 10:16:22 -0800

    Creating a pet without image working, message errors to frontend working, DB detect duplicate pet

--- src/main/java/com/example/mypetyourpet/controller/AuthTest.java
+++ src/main/java/com/example/mypetyourpet/controller/AuthTest.java
@@ -1,27 +1,27 @@
 package com.example.mypetyourpet.controller;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseToken;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/auth")
-public class AuthTest {
-
-    @PostMapping("/verify")
-    public ResponseEntity<String> verifyToken(@RequestBody String idToken) {
-        try {
-            FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
-            String uid = decodedToken.getUid();
-            return ResponseEntity.ok("Token is valid. UID = " + uid);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(401).body("Invalid token");
-        }
-    }
-}
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.auth.FirebaseToken;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api/auth")
+//public class AuthTest {
+//
+//    @PostMapping("/verify")
+//    public ResponseEntity<String> verifyToken(@RequestBody String idToken) {
+//        try {
+//            FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
+//            String uid = decodedToken.getUid();
+//            return ResponseEntity.ok("Token is valid. UID = " + uid);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return ResponseEntity.status(401).body("Invalid token");
+//        }
+//    }
+//}
